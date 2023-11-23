ead385034c53efae38aaabad04ad7cfeae35862a
willqa01 <willqa01@gmail.com>
2023-11-23 16:55:50 +0100

    finex

--- exo1.py
+++ exo1.py
@@ -1,1 +1,40 @@
-from selenium import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+# Spécifiez le chemin vers le fichier chromedriver.exe (assurez-vous de télécharger la version compatible avec votre version de Chrome)
+chrome_driver_path = r"D:\Driver\chromedriver.exe"
+ 
+# Créez une instance du service Chrome
+service = Service(chrome_driver_path)
+
+# Créez une instance du navigateur Chrome avec le service
+driver = webdriver.Chrome(service=service)
+
+# Accédez à une URL
+#driver.get("http://127.0.0.1:5500/test.html")   # direction vers une page web specifique
+driver.get("https://www.google.fr")
+
+# Faites d'autres actions avec le navigateur...
+driver.find_element(By.ID, "L2AGLb").click()
+time.sleep(1)
+driver.find_element(By.ID, "APjFqb").send_keys("acheter un pull femme")
+time.sleep(1)
+driver.find_element(By.ID, "APjFqb").submit()
+time.sleep(1)
+driver.find_element(By.PARTIAL_LINK_TEXT, "Vidéos").click()
+#driver.find_element(By.CSS_SELECTOR, "style").click()
+#driver.find_element(By.LINK_TEXT, "xmlns")
+#driver.find_element(By.CLASS, "Ylm8Fc YmeD8e").click()
+#driver.find_element(By.PARTIAL_LINK_TEXT, "xmlns").click()
+
+#logo = driver.find_element(By.CLASS_SELECTOR, "IHxmle")
+#logo.find_element(By.TAG_NAME, "FMKtTb UqcIub").click()
+
+#driver.get("https://www.google.fr/search?sca_esv=584875080&q=acheter+un+pull+femme&tbm=vid&source=lnms&sa=X&ved=2ahUKEwiR2IS6rtqCAxXUT6QEHeDYAf8Q0pQJegQIDRAB")
+time.sleep(1)
+#driver.find_element(By.ID, "L2AGLb").click()
+
+#driver.find_element(By.ID, "742384653").click()
+input()
--- lancementgoogletest.py
+++ lancementgoogletest.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+import time
 
 
 # Spécifiez le chemin vers le fichier chromedriver.exe (assurez-vous de télécharger la version compatible avec votre version de Chrome)
@@ -18,8 +19,47 @@ driver.get("http://127.0.0.1:5500/test.html")   # direction vers une page web sp
 
 # Faites d'autres actions avec le navigateur...
 
-test = driver.find_element(By.CLASS_NAME, "information")
-test.send_keys("admin@localhost.dev")
-# Fermez le navigateur à la fin     #driver.quit()   # quite le navigateur  ...
-input()
+driver.find_element(By.NAME, "gender").click()
+driver.find_element(By.NAME, "gender").click()
+
+driver.find_element(By.ID, "fname").clear()
+test3 = driver.find_elements(By.ID, "fname")
+for a in test3:
+    a.send_keys("w")
+    time.sleep(2)
+    a.send_keys("i")
+    time.sleep(5)
+    a.send_keys("l")
+    time.sleep(7)
+    a.send_keys("l")
+    time.sleep(2)
+    a.send_keys("y")
+
+test = driver.find_elements(By.CLASS_NAME, "information")
+#test.send_keys("admin@localhost.dev")
+#for i in test:
+    #i.send_keys("Bonjour")
+    #time.sleep(2)
+
+
+driver.find_element(By.ID, "lname").clear()
+test2 = driver.find_elements(By.ID, "lname")
+for y in test2:
+    y.send_keys("D")
+    time.sleep(2)
+    y.send_keys("T")
+    time.sleep(5)
+    y.send_keys("C")
+    time.sleep(7)
+    y.send_keys("    ")
+    time.sleep(2)
+    y.send_keys("merci")
+
+
+driver.find_element(By.NAME, "newsletter").click()
+#driver.find_element(By.ID, "top").click()
+#driver.find_element(By.ID, "will").click()
+
+# Fermez le navigateur à la fin      
+input()     #driver.quit() 
 
