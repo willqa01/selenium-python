baea6403f87aaba5cf996657da8095c22343d1d0
willqa01 <willqa01@gmail.com>
2024-05-08 19:17:03 +0200

    test2

--- exo1.py
+++ exo1.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 import time
 
@@ -14,16 +16,40 @@ driver = webdriver.Chrome(service=service)
 
 # Accédez à une URL
 #driver.get("http://127.0.0.1:5500/test.html")   # direction vers une page web specifique
-driver.get("https://www.google.fr")
-
+driver.get("https://www.cash-piscines.com/")
+#WebDriverWait.until(EC.visibility_of_element_located((driver.get("https://www.cash-piscines.com/"))))
+time.sleep(2)
 # Faites d'autres actions avec le navigateur...
-driver.find_element(By.ID, "L2AGLb").click()
+#driver.find_element(By.ID, "L2AGLb").click()
 #time.sleep(1)
-driver.find_element(By.ID, "APjFqb").send_keys("cash piscines")
+#driver.find_element(By.ID, "APjFqb").send_keys("cash piscines")
 #time.sleep(1)
-driver.find_element(By.ID, "APjFqb").submit()
-time.sleep(1)
-driver.find_element(By.CLASS, "CA5RN").click()
+#driver.find_element(By.ID, "APjFqb").submit()
+#time.sleep(1)
+driver.find_element(By.CLASS_NAME, "mini-action-login").click()
+time.sleep(2)
+#WebDriverWait.until(EC.visibility_of_element_located((By.CLASS_NAME, ""mini-action-login"")))
+#WebDriverWait.until(EC.visibility_of_element_located((By.CLASS_NAME, "mini-action-login")))
+driver.find_element(By.CSS_SELECTOR, "input[type='email']").click()
+time.sleep(2)
+#WebDriverWait.until(EC.presence_of_element_located((By.CLASS_NAME, "input-default")))
+driver.find_element(By.CSS_SELECTOR, "input[type='email']").send_keys("william.bani@it-students.fr")
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "input[type='password']").click()
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "input[type='password']").send_keys("william.bani@it-stud")
+time.sleep(2)
+driver.find_element(By.CLASS_NAME, "btn-lg").click()
+time.sleep(2)
+driver.find_element(By.CLASS_NAME, "search-query").click()
+time.sleep(2)
+driver.find_element(By.CLASS_NAME, "search-query").send_keys("bache")
+#time.sleep(2)
+#driver.find_element(By.CLASS_NAME, "inner").click()
+#time.sleep(7)
+#driver.find_element(By.CLASS_NAME, "input-field").send_keys("william.bani@it-stud")
+
+#WebDriverWait.until(EC.visibility_of_element_located((By.CLASS_NAME, ""mini-action-login"")))
 #driver.find_element(By.PARTIAL_LINK_TEXT, "Vidéos").click()
 #driver.find_element(By.CSS_SELECTOR, "style").click()
 #driver.find_element(By.LINK_TEXT, "xmlns")
@@ -34,7 +60,7 @@ driver.find_element(By.CLASS, "CA5RN").click()
 #logo.find_element(By.TAG_NAME, "FMKtTb UqcIub").click()
 
 #driver.get("https://www.google.fr/search?sca_esv=584875080&q=acheter+un+pull+femme&tbm=vid&source=lnms&sa=X&ved=2ahUKEwiR2IS6rtqCAxXUT6QEHeDYAf8Q0pQJegQIDRAB")
-time.sleep(1)
+#time.sleep(1)
 #driver.find_element(By.ID, "L2AGLb").click()
 
 #driver.find_element(By.ID, "742384653").click()
